d0f83ae432bb985974b9a35d2851574012755800
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-01 00:33:25 +0000

    feat: Adicionando componente Button

---
+++ src/components/button/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ContainerButton} from './styles';
+
+const Button = ({onAction, name}) => {
+  return(
+    <>
+      <ContainerButton  onClick={() => onAction()}>
+        {name}
+      </ContainerButton>
+    </>
+  )
+}
+
+export default Button;
