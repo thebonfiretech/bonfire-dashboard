81f638c6ed999a61479ecea7708a9852b83eaf8c
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:28:52 +0000

    fix: Adicionando  o componente economy

--- src/pages/economy/index.jsx
+++ src/pages/economy/index.jsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React from "react";
 
-const Economy = () =>{
-  return(
-    <>
-    </>
-  )
-}
+import Indicators from "../../components/indicators";
+import { useNavigate } from "react-router-dom";
+import UserNav from "../../components/userNav";
+import Layout from "../../components/layout";
+import Button from "../../components/button";
+import Label from "../../components/label";
+import Input from "../../components/input";
 
+import {
+  Container,
+  Box,
+  IndicatorsContainer,
+  InputsContainer,
+  NavContainer,
+  ButtonContainer,
+} from "./styles";
+
+const Economy = () => {
+  const navigate = useNavigate();
+
+  const dataInvestments = [
+    {
+      name: "poupança",
+      numbers: "50",
+    },
+    {
+      name: "CDI",
+      numbers: "50",
+    },
+    {
+      name: "CDB",
+      numbers: "50",
+    },
+  ];
+
+  const cameBack = () => {
+    localStorage.clear();
+    navigate("/dashboard");
+  };
+
+  return (
+    <Layout>
+      <Container>
+        <NavContainer>
+          <UserNav onAction={cameBack} name="E.M George Chalmers" />
+        </NavContainer>
+        <Box>
+          <Label name="Quantidade de alunos investidores:" />
+          <IndicatorsContainer>
+            {dataInvestments.map((item, index) => {
+              return (
+                <Indicators
+                  name={item?.name}
+                  numbers={item?.numbers}
+                  key={index}
+                />
+              );
+            })}
+          </IndicatorsContainer>
+          <InputsContainer>
+            <Label name="Carteira" />
+            <Input placeholder="lorem ipusum" />
+            <Label name="lorem ipsum" />
+            <Input placeholder="lorem ipusum" />
+          </InputsContainer>
+          <ButtonContainer>
+            <Button name="Atualizar" />
+          </ButtonContainer>
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
 
 export default Economy;
