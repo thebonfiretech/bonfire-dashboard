7fdc378f7a51b59346332467bbeaa663c077f12f
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:29:17 +0000

    fix: Adicionando página de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
+import { AiOutlineUser, AiOutlineUnlock } from "react-icons/ai";
+
+
+import LogoContainer from "../../components/logoContainer";
+import Button from "../../components/button";
+import Input from "../../components/input";
+
+import {Container, Box, InputContainer, Message} from './styles';
 
 const Login = () => {
-  return(
+  const [typePass, setTypePass] = useState("password");
+
+  return (
     <>
+      <Container>
+        <Box>
+          <LogoContainer/>
+          <InputContainer>
+          <Input
+            
+            icon={AiOutlineUser}
+            placeholder='Digíte o seu email:'
+            type={"text"}
+            width="50"
+          />
+          <Input
+            onAction={() =>
+              setTypePass((prev) => (prev == "password" ? "text" : "password"))
+            }
+            icon={AiOutlineUnlock}
+            placeholder="Digíte a sua senha:"
+            type={typePass}
+            width="50"
+          />
+          <Message>
+            <span>senha incorreta.</span>
+            <a>esqueceu a senha?</a>
+          </Message>
+          </InputContainer>
+            <Button name='Entrar'/>
+        </Box>
+      </Container>
     </>
-  )
-}
+  );
+};
 
 export default Login;
