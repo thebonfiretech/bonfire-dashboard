10c7ece8ec59361f63410d0fbce6411645bc3aef
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:15:46 +0000

    fix: Estilização do componente button

---
+++ src/components/button/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ContainerButton = styled.button`
+    background: ${({theme}) => theme.colors.gradient};
+    color: ${({theme}) => theme.colors.text};
+    text-transform: uppercase;
+    box-sizing: border-box;
+    transition: ease .2s;
+    letter-spacing: 1px;
+    font-weight: 800;
+    cursor: pointer;
+    width:  300px;
+    border: none;
+    height: 40px;
+
+  &:hover{
+    opacity: 80%;
+  }
+
+  @media (max-width: 678px) {
+    width: 205px;
+    height: 38px;
+  }
+`
