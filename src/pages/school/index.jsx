28aa2fe1f4ed5ea266ef44d2d1a0767337b4442e
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:25:05 +0000

    fix: Adicionando  o componente school

--- src/pages/school/index.jsx
+++ src/pages/school/index.jsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React from "react";
+
+import Indicators from "../../components/indicators";
+import { useNavigate } from "react-router-dom";
+import UserNav from "../../components/userNav";
+import Layout from "../../components/layout";
+import Button from "../../components/button";
+import Label from "../../components/label";
+import Input from "../../components/input";
+
+import {
+  Container,
+  Box,
+  IndicatorsContainer,
+  InputsContainer,
+  NavContainer,
+  ButtonContainer,
+} from "./styles";
 
 const School = () => {
-  return(
-    <>
-    </>
-  )
-}
+  const navigate = useNavigate();
+
+  const dataIndicators = [
+    {
+      name: "alunos",
+      numbers: "600",
+    },
+    {
+      name: "responsáveis",
+      numbers: "3",
+    },
+    {
+      name: "professores",
+      numbers: "20",
+    },
+  ];
+
+  const cameBack = () => {
+    localStorage.clear();
+    navigate("/dashboard");
+  };
+
+  return (
+    <Layout>
+      <Container>
+        <NavContainer>
+          <UserNav onAction={cameBack} name="E.M George Chalmers" />
+        </NavContainer>
+        <Box>
+          <Label name="Quantidade de usuarios logados:" />
+          <IndicatorsContainer>
+            {dataIndicators.map((item, index) => {
+              return (
+                <Indicators
+                  name={item?.name}
+                  numbers={item?.numbers}
+                  key={index}
+                />
+              );
+            })}
+          </IndicatorsContainer>
+          <InputsContainer>
+            <Label name="lorem ipsum" />
+            <Input placeholder="lorem ipusum" />
+            <Label name="lorem ipsum" />
+            <Input placeholder="lorem ipusum" />
+          </InputsContainer>
+          <ButtonContainer>
+            <Button name="Atualizar" />
+          </ButtonContainer>
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
 
 export default School;
