54337a5b12d49f0b62768d771b39b1d9d7547384
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:25:25 +0000

    fix: Adicionando  o componente userNav

---
+++ src/components/userNav/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {AiOutlineCaretLeft} from 'react-icons/ai';
+import {Container, Button} from './styles';
+
+const UserNav = ({name, onAction}) => {
+  return(
+    <>
+      <Container>
+            <Button onClick={() => onAction()}><AiOutlineCaretLeft color='#fff' size={24}/></Button>
+           <h1>{name}</h1>
+      </Container>
+    </>
+  )
+}
+
+export default UserNav;
