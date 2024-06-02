c347931de52fbea6c4ded163a1c709036ec50cd4
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:16:22 +0000

    fix: Correções do componente filterButtons

--- src/components/filterButtons/index.jsx
+++ src/components/filterButtons/index.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import {ContainerButton} from './styles';
+import React from "react";
+import { ContainerButton } from "./styles";
 
-const FilterButtons = ({onAction, name, select}) => {
-  return(
+const FilterButtons = ({ onAction, name, select }) => {
+  return (
     <>
-      <ContainerButton  onClick={onAction}>
-        {name}
-      </ContainerButton>
+      <ContainerButton select={select} name={name} onClick={onAction}>{name}</ContainerButton>
     </>
-  )
-}
+  );
+};
 
 export default FilterButtons;
