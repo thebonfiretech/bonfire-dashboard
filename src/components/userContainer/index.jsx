18f3ddffbe7691d6aaef36fb7adcc5c3eb2b4282
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:24:28 +0000

    fix: Adicionando  o componente userConatiner

---
+++ src/components/userContainer/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {MdDriveFileRenameOutline, MdDeleteOutline} from 'react-icons/md';
+
+import {Container, UserInformation, ButtonIcon, ControlPanel, Button} from './styles';
+
+const UserContainer = ({name, iconType: IconType}) => {
+  const iconSize = window.innerWidth <= 678 ? 21 : 24;
+  return(
+    <>
+      <Container>
+        <UserInformation>
+            <ButtonIcon onClick={() => onAction()}> {IconType && <IconType size={iconSize} color={"#FFFFFF"}/>}</ButtonIcon>
+            <h1>{name}</h1>
+        </UserInformation>
+       <ControlPanel>
+           <Button onClick={() => onAction()}><MdDriveFileRenameOutline color='#fff' size={iconSize}/></Button>
+         <Button onClick={() => onAction()}><MdDeleteOutline color='#fff' size={iconSize}/></Button>
+       </ControlPanel>
+      </Container>
+    </>
+  )
+}
+
+export default UserContainer;
