c5655b5354807254c2f7bf6fb62d7fa4cde940f6
Gabriela Viana <108141404+Gabi-Viana@users.noreply.github.com>
2024-06-02 16:32:09 +0000

    fix: Atualização da página de erro

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-
-import { Container, Box } from './styles';
-
+import { AiOutlineClose } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/button";
+import { Container, Box, Position1, Position2, Position3, Message, Icon } from './styles';
 
 const Error = () => {
+    const navigate = useNavigate();
 
+    const cameBack = () => {
+        navigate('/dashboard')
+    }
     return (
         <Container>
             <Box>
-                <p>error page</p>
+                <Position1>
+                    <Icon className='icon'>
+                        <AiOutlineClose size={30} color='#fff'/>
+                    </Icon>
+                </Position1>
+                <h1>Oops!</h1>
+                <Message>
+                <Position2>
+                    <Icon className='icon'>
+                        <AiOutlineClose size={30} color='#fff'/>
+                    </Icon>
+                </Position2>
+                    <p>Algo deu errado</p>
+                </Message>
+                <Button name='TENTE NOVAMENTE' onAction={cameBack}/>
+                    <Position3>
+                        <Icon className='icon'>
+                            <AiOutlineClose size={30} color='#fff'/>
+                        </Icon>
+                    </Position3>
             </Box>
         </Container>
-    )
+    );
 }
 
-export default Error
+export default Error;
