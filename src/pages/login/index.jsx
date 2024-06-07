6a08652eac1b5858d4618bc2da963ed119740ee0
Adrian Jonathan <159457932+AdrianJonathanMB@users.noreply.github.com>
2024-06-07 17:24:00 +0000

    feat Página de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,45 +1,111 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineUser, AiOutlineUnlock } from "react-icons/ai";
-
-
+import ActionsBase from "../../actions/base";
+import ActionsAdmin from "../../actions/admin/user";
 import LogoContainer from "../../components/logoContainer";
 import Button from "../../components/button";
 import Input from "../../components/input";
+import { useNavigate } from "react-router-dom";
+import Alert from '../../components/alert'
 
-import {Container, Box, InputContainer, Message} from './styles';
+import { Container, Box, InputContainer, Message } from "./styles";
 
 const Login = () => {
   const [typePass, setTypePass] = useState("password");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [alert, setAlert] = useState(undefined);
+
+  class User {
+    constructor(id, password) {
+      this.id = id;
+      this.password = password;
+    }
+  }
+
+  useEffect(() => {
+    localStorage.removeItem("token");
+  }, []);
+
+  const LoginUser = async (user) => {
+    if (!id || !password) {
+      setAlert({
+        icon: "info",
+        title: "Preencha todos os campos",
+        confirm: false,
+        timer: 1500,
+      });
+      return;
+    }
+
+    try {
+      await ActionsBase.login(user)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+ 
+  const clearAlert = () => {
+    if (alert) {
+      setAlert(undefined);
+    }
+  };
 
   return (
     <>
       <Container>
         <Box>
-          <LogoContainer/>
+          <LogoContainer />
           <InputContainer>
-          <Input
-            
-            icon={AiOutlineUser}
-            placeholder='Digíte o seu email:'
-            type={"text"}
-            width="50"
-          />
-          <Input
-            onAction={() =>
-              setTypePass((prev) => (prev == "password" ? "text" : "password"))
-            }
-            icon={AiOutlineUnlock}
-            placeholder="Digíte a sua senha:"
-            type={typePass}
-            width="50"
-          />
-          <Message>
-            <span>senha incorreta.</span>
-            <a>esqueceu a senha?</a>
-          </Message>
+            <Input
+              icon={AiOutlineUser}
+              onChange={(value) => {
+                setId(value);
+                clearAlert(); 
+              }}
+              placeholder="Digite o seu usuário:"
+              type={"text"}
+              value={id}
+              width="50"
+            />
+            <Input
+              onAction={() =>
+                setTypePass((prev) =>
+                  prev === "password" ? "text" : "password"
+                )
+              }
+              icon={AiOutlineUnlock}
+              onChange={(value) => {
+                setPassword(value);
+                clearAlert(); 
+              }}
+              placeholder="Digite a sua senha:"
+              type={typePass}
+              value={password}
+              width="50"
+            />
+            <Message>
+              <span>{message}</span>
+            </Message>
           </InputContainer>
-            <Button name='Entrar'/>
+          <Button
+            name="Entrar"
+            onAction={() => {
+              const user = new User(id, password);
+              LoginUser(user);
+            }}
+          />
         </Box>
+        {alert && (
+          <Alert
+            icon={alert.icon}
+            title={alert.title}
+            timer={alert.timer}
+            confirm={alert.confirm}
+          />
+        )}
       </Container>
     </>
   );
