0f24c40e2c48af7451d462542e25388602d995d7
Adrian Jonathan <159457932+AdrianJonathanMB@users.noreply.github.com>
2024-06-07 17:25:53 +0000

    feat: página dashboard com verificação de token

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Layout from "../../components/layout";
 
 const Dashboard = () => {
+  useEffect(() => {
+   const token = localStorage.getItem("token")
+      if(!token) {
+        navigate("/") 
+        return
+      }
+  }, []);
+  
   return (
     <Layout>
     
