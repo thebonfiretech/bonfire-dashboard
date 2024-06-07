7e1bc1dfa6dd4d6b46c9cf0e5477c9472ba37f0c
Adrian Jonathan <159457932+AdrianJonathanMB@users.noreply.github.com>
2024-06-07 17:21:59 +0000

    feat: página de usuários

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,64 +1,131 @@
 import Layout from "../../components/layout";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Box, Items } from "./styles";
 import { AiOutlineUser, AiOutlineRocket, AiOutlineTool } from "react-icons/ai";
 import FilterButtons from "../../components/filterButtons";
 import SearchBar from "../../components/searchBar";
 import UserContainer from "../../components/userContainer";
+import Actions from "../../actions/admin/user";
+import Loader from '../../components/loader';
+import { useNavigate } from "react-router-dom";
+import Alert from '../../components/alert';
 
 const Users = () => {
+
+  const [selected, setSelected] = useState("normal");
+  const [search, setSearch] = useState("");
+  const [removeLoading, setRemoveLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const get = async () => {
+      if (!token) {
+        navigate("/");
+        return;
+      }
+      try {
+        const response = await Actions.getAll();
+        setRemoveLoading(true);
+        setUsers(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    get();
+  }, []);
+
   
+const [alert, setAlert] = useState(undefined);
+
+  const deleteUser = async (id) => {
+
+    try {
+      await Actions.deleteUser(id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const confirmDeleteUser = (id) => {
+    if (!alert) {
+      setAlert({
+        icon: "info",
+        title: "Deseja Deletar este usuário?",
+        confirm: true,
+        cancel: true,
+        timer: false,
+        onConfirm: () => deleteUser(id),
+        onCancel: () => setAlert(undefined),
+      });
+    }
+  };
+
   const dataFilter = [
     {
-      name: "Usuários",
+      name: "normal",
     },
     {
-      name: "Administradores",
+      name: "administrador",
     },
     {
-      name: "Responsáveis",
+      name: "professor",
     },
   ];
 
-  const dataUsers = [
-    {
-      name: "José",
-      iconType: AiOutlineUser,
-    },
-    {
-      name: "Pedro",
-      iconType: AiOutlineRocket,
-    },
-    {
-      name: "Carlos",
-      iconType: AiOutlineTool,
-    },
-  ];
 
-  const [selected, setSelected] = useState(undefined)
   return (
     <Layout>
       <Container>
         <Box>
-          <SearchBar />
+          <SearchBar onChange={(value) => setSearch(value)} />
           <section>
             {dataFilter.map((item, index) => {
-              return <FilterButtons select={ selected } onAction={() => setSelected((prev) => prev == item?.name ? undefined : item?.name)} name={item?.name} key={index} />;
-            })}
-          </section>
-          <Items>
-            {dataUsers.map((item, index) => {
               return (
-                <UserContainer
+                <FilterButtons
+                  select={selected}
+                  onAction={() =>
+                    setSelected((prev) =>
+                      prev == item?.name ? undefined : item?.name,
+                    )
+                  }
                   name={item?.name}
-                  iconType={item?.iconType}
                   key={index}
                 />
               );
             })}
+          </section>
+          <Items>
+            {users
+              .filter(
+                (item) =>
+                  item?.role == selected && item?.name.startsWith(search),
+              )
+              .reverse()
+              .map((item, index) => {
+                return (
+                  <UserContainer
+                    onUpdate={() => navigate(`/users/edit/${item?._id}`)}
+                    onDelete={() => confirmDeleteUser(item?._id)}
+                    name={item?.name}
+                    iconType={
+                      item?.role == "normal"
+                        ? AiOutlineUser
+                        : item?.role == "administrador"
+                          ? AiOutlineRocket
+                          : AiOutlineTool
+                    }
+                    key={index}
+                  />
+                );
+              })}
           </Items>
         </Box>
       </Container>
+      {alert && <Alert icon={alert.icon} title={alert.title} timer={alert.timer} confirm={alert.confirm} cancel={alert.cancel} onConfirm={alert.onConfirm} onCancel={alert.onCancel}/>}
+       {!removeLoading && <Loader />}
     </Layout>
   );
 };
