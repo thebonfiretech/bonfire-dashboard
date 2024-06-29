ad6bbe94aed84ef2e0b7f1bb7bd0e4690f927e47
Adrian Jonathan <159457932+AdrianJonathanMB@users.noreply.github.com>
2024-06-29 00:31:37 +0000

    refactor: mudança na verificação do token

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,16 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import Background from "../background"
-import { Container } from './styles'
-import Menu from '../menu'
-const Layout = ({children}) => {
+import React, { useState, useEffect } from "react";
+import { Container } from "./styles";
+import Background from "../background";
+import Menu from "../menu";
+import { useNavigate } from "react-router-dom";
+
+const Layout = ({ children }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+      return;
+    }
+  }, []);
+  
   return (
     <Container>
-      <Menu/>
-    <Background>
-      {children}
-    </Background>
+      <Menu />
+      <Background>{children}</Background>
     </Container>
-  )
-}
+  );
+};
 
 export default Layout;
