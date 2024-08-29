78ed9b1be02bafc1c831e9c743d4d16edd28d305
Gabi-Viana <gabivitoria896@hotmail.com>
2024-08-29 17:57:07 -0300

    feat: env config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ command, mode }) => {
-  
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
   return {
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
     plugins: [react()],
     server: {
       host: '0.0.0.0',
-      port: '8080'
+      port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 3000,
     },
-    build:{
-      outDir: 'build'
-    }
-  }
-})
+    build: {
+      outDir: 'build',
+    },
+  };
+});
